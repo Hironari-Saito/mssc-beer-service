e8fdf6d5e855be2fa1a55d879a9ab74d03cfcb84
Hironari-Saito <hiroshi31100@gmail.com>
2020-12-13 04:26:54 +0900

    end of section 17

--- src/main/java/guru/sfg/brewery/model/events/BrewBeerEvent.java
+++ src/main/java/guru/sfg/brewery/model/events/BrewBeerEvent.java
@@ -4,6 +4,7 @@ import guru.sfg.brewery.model.BeerDto;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+
 public class BrewBeerEvent extends BeerEvent{
 
     public BrewBeerEvent(BeerDto beerDto) {
